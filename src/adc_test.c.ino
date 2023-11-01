00fb03efa0262bd25624812b887c693209265932
AhmedSamy2004 <104323652+AhmedSamy2004@users.noreply.github.com>
2023-11-01 17:17:43 +0200

    character adding

--- src/adc_test.c.ino
+++ src/adc_test.c.ino
@@ -9,14 +9,15 @@ int main(){
     //test ADC_init function
     Adc_Init();
     assert(ADMUX & (1<<REFS0) !=0);
-    uart_SendString("REFS0 is correct",);
+    uart_SendString("REFS0 is correct",16);
     assert(ADCSRA & (1<<ADEN) !=0 );
-    uart_SendString("ADEN is correct",);
+    uart_SendString("ADEN is correct",15);
     assert(ADCSRA & (1<<ADPS2) !=0 );
-    uart_SendString("ADPS2 is correct",);
+    uart_SendString("ADPS2 is correct",16);
     assert(ADCSRA & (1<<ADPS1) !=0 );
-    uart_SendString("ADPS1 is correct",);
+    uart_SendString("ADPS1 is correct",16);
     assert(ADCSRA & (1<<ADPS0) !=0 );
+    uart_SendString("ADPS0 is correct",16);
     
   }
 }
